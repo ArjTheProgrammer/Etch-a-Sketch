6fb2a30f1d9d424b01ce27565f0c1824542e9d88
Jose Neil Silagan <neilsilagan@gmail.com>
2024-05-12 13:19:58 +0800

    add the clear grid function to fix the bug that creates multiple grids

--- javascript.js
+++ javascript.js
@@ -1,23 +1,33 @@
 const grids = document.querySelector(".grids");
 const slider = document.querySelector(".grid-slider");
 
+const displaySliderValue = document.querySelector(".display-slider-value");
+displaySliderValue.textContent = `Grids: ${slider.value}`;
+
+slider.addEventListener("input", () => {
+    clearGrid();
+    displaySliderValue.textContent = `Grids: ${slider.value}`;
+    addGrids(slider.value);
+})
 
 function addGrids(N){
     let totalN = N * N;
-    let sideLength = grids.offsetWidth / N;
+    let width = grids.offsetWidth / N;
+
+    let height = grids.offsetHeight / N;
 
     for (let i = 1; i <= totalN; i++){
         const grid = document.createElement("div");
         grid.classList.add("grid");
         grid.style.cursor = "cross-hair";
     
-        grid.style.height = `${sideLength}px`;
-        grid.style.width = `${sideLength}px`;
+        grid.style.height = `${height}px`;
+        grid.style.width = `${width}px`;
 
         grids.appendChild(grid);
     }
 }
 
-addGrids(slider.value);
-
-console.log(slider.value);
+function clearGrid(){
+    grids.innerHTML = "";
+}
